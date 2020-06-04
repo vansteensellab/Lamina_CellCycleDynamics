64920d4cdf0007ec2edf4f33805fc3281aeeb02d
tomvanschaik <t.v.schaik@nki.nl>
2020-06-04 17:26:24 +0200

    Upload sequencing scripts
    Uploaded from the virtual machine (Windows), using the github website

---
+++ Sequencing/rnaseq/processing_pipeline/bdg_to_bw.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+# check commands: 
+# slopBed, bedGraphToBigWig and bedClip
+
+which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
+which bedGraphToBigWig &>/dev/null || { echo "bedGraphToBigWig not found! Download: <http://hgdownload.cse.ucsc.edu/admin/exe/>"; exit 1; }
+which bedClip &>/dev/null || { echo "bedClip not found! Download: <http://hgdownload.cse.ucsc.edu/admin/exe/>"; exit 1; }
+
+# check input
+if [ $# -lt 2 ];then
+    echo "Need 2 parameters! <bedgraph> <chrom info>"
+    exit
+fi
+
+# convert
+F=$1
+G=$2
+
+bedtools slop -i ${F} -g ${G} -b 0 | bedClip stdin ${G} ${F}.clip
+
+LC_COLLATE=C sort -k1,1 -k2,2n ${F}.clip > ${F}.sort.clip
+
+bedGraphToBigWig ${F}.sort.clip ${G} ${F/bg/bw}
+
+rm -f ${F}.clip ${F}.sort.clip
+
